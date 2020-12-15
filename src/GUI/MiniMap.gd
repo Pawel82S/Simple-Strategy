3a07cf71b2d13b942d73a6a33c04ab6a2010fb30
Paweł S <theoptimizersgroup@gmail.com>
2020-12-15 15:24:56 +0100

    MinMap code cleanup and reorganization.

--- src/GUI/MiniMap.gd
+++ src/GUI/MiniMap.gd
@@ -24,11 +24,14 @@ func update() -> void:
 	for child in grid.get_children():
 		grid.remove_child(child)
 	
+	_display_system_icons()
+	_display_system_connections()
+
+################################################################# PRIVATE METHODS ########################################################
+func _display_system_icons() -> void:
 	var grid_x_factor = grid.rect_size.x / Data.galaxy_rect.size.x
 	var grid_y_factor = grid.rect_size.y / Data.galaxy_rect.size.y
-#	print("Factor x: %f, factor y: %f" % [grid_x_factor, grid_y_factor])
 	var grid_center_offset: Vector2 = grid.rect_size / 2
-#	print("Grid center offset: %s" % grid_center_offset)
 	for sys_pos in Data.system_positions:
 		var grid_pos: Vector2 = sys_pos
 		grid_pos.x *= grid_x_factor
@@ -37,14 +40,14 @@ func update() -> void:
 		var new_sprite := sprite.duplicate()
 		grid.add_child(new_sprite)
 		new_sprite.position = grid_pos
-#		print(grid_pos)
 		new_sprite.show()
-	
+
+
+func _display_system_connections() -> void:
 	var planet_sprites := grid.get_children()
 	for id in range(Data.system_positions.size()):
 		var from_pos: Vector2 = grid.get_child(id).position
 		var system_connections := Data.system_path.get_point_connections(id)
-#		print("System %d, has %d connections" % [id, system_connections.size()])
 		for connection_id in system_connections:
 			if connection_id < id:
 				continue
@@ -55,5 +58,3 @@ func update() -> void:
 			var to_pos: Vector2 = grid.get_child(connection_id).position
 			new_line.add_point(to_pos)
 			new_line.show()
-
-################################################################# PRIVATE METHODS ########################################################
